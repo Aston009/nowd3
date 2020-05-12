3776b94258755829055d13c3fbf347c9b7abe481
Arthur <Gameart009@mail.ru>
2020-05-12 21:33:05 +0900

    first commit

---
+++ precache-manifest.e2f1674ea968db5ca8e3e44ac9cd7649.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f5d2750d6da29ba1b84d34bf4415297",
+    "url": "/index.html"
+  },
+  {
+    "revision": "143f38a520ac6f89f557",
+    "url": "/static/css/main.2f731515.chunk.css"
+  },
+  {
+    "revision": "d2f7960d078bda80443c",
+    "url": "/static/js/2.a195bfb1.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.a195bfb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "143f38a520ac6f89f557",
+    "url": "/static/js/main.2f785882.chunk.js"
+  },
+  {
+    "revision": "1187d537cfc5dff2db3a",
+    "url": "/static/js/runtime-main.8d0417a9.js"
+  },
+  {
+    "revision": "cdc95f38bc0d88e1f24de507c403487c",
+    "url": "/static/media/plus.cdc95f38.svg"
+  }
+]);
